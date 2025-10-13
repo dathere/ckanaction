4fc9937520b4f9ca8ac20d8717255a378b1c2ee1
rzmk <30333942+rzmk@users.noreply.github.com>
2025-10-12 21:13:17 -0400

    feat(docs): add GIF, GitHub URL, and more endpoints

--- docs/lib/layout.shared.tsx
+++ docs/lib/layout.shared.tsx
@@ -20,5 +20,6 @@ export function baseOptions(): BaseLayoutProps {
     },
     // see https://fumadocs.dev/docs/ui/navigation/links
     links: [],
+    githubUrl: "https://github.com/dathere/ckanaction"
   };
 }
