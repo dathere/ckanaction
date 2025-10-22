0eaf2eb00048693b4d3a704ba8f18eebf5442941
rzmk <30333942+rzmk@users.noreply.github.com>
2025-10-22 06:40:35 -0400

    build(docs): use latest nextjs and fumadocs with default unset value fix

--- docs/lib/openapi.ts
+++ docs/lib/openapi.ts
@@ -2,38 +2,4 @@ import { createOpenAPI } from 'fumadocs-openapi/server';
 
 export const openapi = createOpenAPI({
   input: ["./lib/openapi.yml"],
-  generateCodeSamples(endpoint) {
-    return [
-      {
-        lang: 'curl',
-        label: 'cURL',
-        source: false,
-      },
-      {
-        lang: 'javascript',
-        label: 'JavaScript',
-        source: false,
-      },
-      {
-        lang: 'go',
-        label: 'Go',
-        source: false,
-      },
-      {
-        lang: 'python',
-        label: 'Python',
-        source: false,
-      },
-      {
-        lang: 'java',
-        label: 'Java',
-        source: false,
-      },
-      {
-        lang: 'c#',
-        label: 'C#',
-        source: false,
-      },
-    ];
-  },
 });
