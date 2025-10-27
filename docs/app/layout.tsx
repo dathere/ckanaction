b0f2d3e8de1558ea7dd42c5f4a7622719ce02e00
rzmk <30333942+rzmk@users.noreply.github.com>
2025-10-27 16:22:07 -0400

    chore(docs): attempt putting script within RootProvider

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -11,12 +11,14 @@ export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-        <Script
-          src="https://mk-analytics.dathere.com/api/script.js"
-          data-site-id="10"
-          strategy="afterInteractive"
-        />
+        <RootProvider>
+          {children}
+          <Script
+            src="https://mk-analytics.dathere.com/api/script.js"
+            data-site-id="10"
+            strategy="afterInteractive"
+          />
+        </RootProvider>
       </body>
     </html>
   );
