c53511657fd1d70fdb9fe79e49fdaa7bd1c8155c
rzmk <30333942+rzmk@users.noreply.github.com>
2025-12-15 12:36:59 -0500

    fix: mobile title

--- docs/app/(home)/page.tsx
+++ docs/app/(home)/page.tsx
@@ -100,7 +100,7 @@ function Hero() {
             "repeating-linear-gradient(65deg, var(--color-purple-300), var(--color-purple-300) 12px, color-mix(in oklab, var(--color-blue-600) 30%, transparent) 20px, transparent 200px)",
         }}
       />
-      <h1 className="mb-8 text-4xl font-medium md:hidden">ckan-devstaller</h1>
+      <h1 className="mb-8 text-4xl font-medium md:hidden">ckanaction</h1>
       <h1 className="mb-8 max-w-[800px] text-4xl font-medium max-md:hidden">
         <span className="text-5xl">
           ckanaction <GiftIcon className="inline-block w-10 h-10 pb-1" />
