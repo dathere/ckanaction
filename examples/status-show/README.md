723130792c96109691518a19aa2652029a63b71d
rzmk <30333942+rzmk@users.noreply.github.com>
2026-02-03 18:13:21 -0500

    feat: add example of using status_show method

---
+++ examples/status-show/README.md
@@ -0,0 +1,9 @@
+# Example of using ckanaction to run the /status_show API endpoint
+
+## Usage
+
+Run the following command to run the `/status_show` CKAN Action API endpoint on `http://localhost:5000` and print the formatted debug output.
+
+```bash
+cargo run
+```
---
+++ examples/status-show/src/main.rs
@@ -0,0 +1,14 @@
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // Initialize and build CKAN struct
+    let ckan = ckanaction::CKAN::builder()
+        .url("http://localhost:5000")
+        .build();
+
+    // Send request to /status_show and print formatted debug output
+    let status_show = ckan.status_show().await?;
+    println!("{status_show:#?}");
+
+    Ok(())
+}
